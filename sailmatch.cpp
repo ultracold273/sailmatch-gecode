b872c7cc74514a44c8422e862f51157885a4f228
Wei Lx <lx@WeideMacBook-Pro.local>
2015-12-12 01:00:27 +0800

    author info updated

--- sailmatch.cpp
+++ sailmatch.cpp
@@ -1,6 +1,6 @@
 /**
- * Author: 
- * SID:
+ * Author: Tian Ye
+ * SID: 1155073201
  *
  * CSCI 5240 Project
  *
@@ -13,52 +13,68 @@
 #include <gecode/int.hh>
 #include <gecode/search.hh>
 #include <gecode/driver.hh>
-#include <string>
-#include <sstream>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
 
 using namespace Gecode;
 
 class SailMatchOptions : public Options {
+private:
+	int numboats;
+	int numskippers;
 public:
-	int boats;
-	int skippers;
-	SailMatchOptions(const char * s, int boats0, int skippers0) : Options(s), boats(boats0), skippers(skippers0){}
+	SailMatchOptions(const char * s, int boats0, int skippers0) : Options(s), numboats(boats0), numskippers(skippers0){}
 	void parse(int& argc, char * argv[]) {
 		Options::parse(argc, argv);
-		std::istringstream strInt;
 		for(int i = 0;i < argc;i++) {
-			std::string strArg(argv[i]);
-			if (strArg == "-boats" && i + 1 < argc) {
-				strInt.str(argv[i+1]);
-				if (!(strInt >> boats)) {
+			if (!strncmp(argv[i], "-boats", 7) && i + 1 < argc) {
+				numboats = atoi(argv[i+1]);
+				if (numboats == 0) {
 					std::cerr << "Invalid Argument for boats" << argv[i + 1] << std::endl;
 				}
 				i++;
 			}
-			if (strArg == "-skippers" && i + 1 < argc) {
-				strInt.str(argv[i+1]);
-				if (!(strInt >> skippers)) {
+			if (!strncmp(argv[i], "-skippers", 10) && i + 1 < argc) {
+				numskippers = atoi(argv[i+1]);
+				if (numskippers == 0) {
 					std::cerr << "Invalid Argument for skippers" << argv[i + 1] << std::endl;
 				}
 				i++;
 			}
 		}
 	}
-}
+	inline int boats() const {return numboats;}
+	inline int skippers() const {return numskippers;}
+};
 
 
 class SailMatch : public Script {
-proctected:
-	IntVarArray timeSlots;
+protected:
 	const SailMatchOptions& opt;
+	IntVarArray timeSlots;
 public:
-	SailMatch(const SailMatchOptions& opt) {
+	SailMatch(const SailMatchOptions& _opt) : Script(_opt), opt(_opt), timeSlots(*this, 1) {
+		std::cout << "Boats Num: " << opt.boats() << std::endl;
+		std::cout << "Skippers Num: " << opt.skippers() << std::endl;
 	}
-}
+	// Search Support
+	SailMatch(bool share, SailMatch& s) : Script(share, s), opt(s.opt) {
+		timeSlots.update(*this, share, s.timeSlots);
+	}
+	virtual Script * copy(bool share) {
+		return new SailMatch(share, *this);
+	}
+	// print Solution
+	void print(std::ostream& os) const {
+		os << "Boats Num: " << opt.boats() << std::endl;
+		os << "Skippers Num: " << opt.skippers() << std::endl;
+	}
+};
 
 int main(int argc, char * argv[]) {
 	SailMatchOptions opt("SailMatchScheduling", 8, 8);
-	opt.solutions(0);
+	opt.solutions(1);
 	opt.parse(argc, argv);
 
 	Script::run<SailMatch, BAB, SailMatchOptions>(opt);
