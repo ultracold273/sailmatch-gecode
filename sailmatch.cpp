3026f9f9a2a90fc4b97fe0ed1ab3681d39ba338d
Wei Lx <lx@WeideMacBook-Pro.local>
2015-12-11 23:55:59 +0800

    fin options

--- sailmatch.cpp
+++ sailmatch.cpp
@@ -14,6 +14,7 @@
 #include <gecode/search.hh>
 #include <gecode/driver.hh>
 #include <string>
+#include <sstream>
 
 using namespace Gecode;
 
@@ -24,8 +25,23 @@ public:
 	SailMatchOptions(const char * s, int boats0, int skippers0) : Options(s), boats(boats0), skippers(skippers0){}
 	void parse(int& argc, char * argv[]) {
 		Options::parse(argc, argv);
+		std::istringstream strInt;
 		for(int i = 0;i < argc;i++) {
-			if (argv[i] )
+			std::string strArg(argv[i]);
+			if (strArg == "-boats" && i + 1 < argc) {
+				strInt.str(argv[i+1]);
+				if (!(strInt >> boats)) {
+					std::cerr << "Invalid Argument for boats" << argv[i + 1] << std::endl;
+				}
+				i++;
+			}
+			if (strArg == "-skippers" && i + 1 < argc) {
+				strInt.str(argv[i+1]);
+				if (!(strInt >> skippers)) {
+					std::cerr << "Invalid Argument for skippers" << argv[i + 1] << std::endl;
+				}
+				i++;
+			}
 		}
 	}
 }
@@ -33,12 +49,18 @@ public:
 
 class SailMatch : public Script {
 proctected:
-	IntVarArray ;
+	IntVarArray timeSlots;
+	const SailMatchOptions& opt;
 public:
-	SailMatch() {
+	SailMatch(const SailMatchOptions& opt) {
 	}
 }
 
 int main(int argc, char * argv[]) {
+	SailMatchOptions opt("SailMatchScheduling", 8, 8);
+	opt.solutions(0);
+	opt.parse(argc, argv);
+
+	Script::run<SailMatch, BAB, SailMatchOptions>(opt);
 	return 0;
 }
